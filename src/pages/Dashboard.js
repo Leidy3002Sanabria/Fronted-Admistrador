34c89935a2f1c7915685be6e704b03b749a71fcc
Yeisonata <104098331+Yeisonata@users.noreply.github.com>
2024-04-16 10:24:06 -0500

    Implementacion del dashboard y graficas

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,54 +1,91 @@
 import React from "react";
 import { BsArrowDownRight } from "react-icons/bs";
-import { Column } from '@ant-design/plots';
-const Dashboard = ()=> {
-    return(
-        <div>
-            <h3 className="mb-4">Dashboard</h3>
-            <div className="d-flex justify-content-between aling-items-center gap-3">
-            
-            <div className="d-flex  justify-content-between align-items-end flex-grow-1 bg-white p-3 roundned-3">
-                <div>
-                    <p className="mb-0">Total</p> 
-                    <h4 className="mb-0 sub-title">$100</h4>
-                </div>
-                <div className="d-flex flex-column align-items-end">
-                <h6 classname="green"><BsArrowDownRight />32%</h6>
-                    <p className="mb-0">
-                    Compare tu April
-                    </p>
-                </div>
-            </div> 
-            <div className="d-flex  justify-content-between align-items-end flex-grow-1 bg-white p-3 roundned-3">
-                <div>
-                    <p className="mb-0">Total</p> 
-                    <h4 className="mb-0 sub-title">$100</h4>
-                </div>
-                <div className="d-flex flex-column align-items-end">
-                <h6 classname="red"><BsArrowDownRight />32%</h6>
-                    <p className="mb-0">
-                    Compare tu April
-                    </p>
-                </div>
-            </div> 
-            <div className="d-flex  justify-content-between align-items-end flex-grow-1 bg-white p-3 roundned-3">
-                <div>
-                    <p className="mb-0">Total</p> 
-                    <h4 className="mb-0 sub-title">$100</h4>
-                </div> 
-                <div className="d-flex flex-column align-items-end">
-                    <h6 classname="green"><BsArrowDownRight />32%</h6>
-                    <p className="mb-0">
-                    Compare tu April
-                    </p>
-                </div>
-            </div>
-
-            </div>
-            
-
+import { Column } from "@ant-design/plots";
+const data = [
+  { type: "1-3秒", value: 0.16 },
+  { type: "4-10秒", value: 0.125 },
+  { type: "11-30秒", value: 0.24 },
+  { type: "31-60秒", value: 0.19 },
+  { type: "1-3分", value: 0.22 },
+  { type: "3-10分", value: 0.05 },
+  { type: "10-30分", value: 0.01 },
+  { type: "30+分", value: 0.015 },
+];
+const Dashboard = () => {
+  const config = {
+    data,
+    xField: "type",
+    yField: "value",
+    style: {
+      fill: ({ type }) => {
+      
+        return "#524CC2";
+      },
+    },
+    label: {
+      text: (originData) => {
+        const val = parseFloat(originData.value);
+        if (val < 0.05) {
+          return (val * 100).toFixed(1) + "%";
+        }
+        return "";
+      },
+      offset: 10,
+    },
+    legend: false,
+  };
+  return (
+    <div>
+      <h3 className="mb-4">Dashboard</h3>
+      <div className="d-flex justify-content-between align-items-center gap-3">
+        <div className="d-flex  justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p>
+            <h4 className="mb-0 sub-title">$100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0">Compare tu April</p>
+          </div>
         </div>
-       
-    )
-}
-export default Dashboard ;
+        <div className="d-flex  justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p>
+            <h4 className="mb-0 sub-title">$100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0">Compare tu April</p>
+          </div>
+        </div>
+        <div className="d-flex  justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p>
+            <h4 className="mb-0 sub-title">$100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0">Compare tu April</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <h3 className="mb-4">Ingresos estáticos</h3>
+        <div>
+          <Column {...config} />;
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,25 +5,20 @@ const Login = () => {
     return (
         
         <div className="py-5" style={{background:"#6C63FF", minHeight:"100vh"}}>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
 
         <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
             <h3 className="text-center">Login</h3>
             <p className="text-center">Ingresa tus datos registrados, para hacer uso de nuestro servicio.</p>
         <form action="">
-           <CustomInput type ="text" Label="EmailAddress" id="email"/>
+           <CustomInput type ="text" label="EmailAddress"id="email"/>
             <CustomInput type= "password" label="password" id="pass"/>
             <div className='mb-3 text-end'>
 
                 <Link to="forgot-password" className="">Recuperar contraseña</Link>
             </div>
             <Link to= "/admin"
-            className="border-0 px-3 py-2 text- white fw-bold w-100 text-center text-decoration-none fs-5"
-            style={{background:"6C63FF"}}
+            className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5 color-button"
+            
             type="submit">
                 Login
             </Link>
